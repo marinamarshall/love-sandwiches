2eba402e27ffd3cf70d3de665001e378a4bf172f
marinamarshall <marinamarshall2@gmail.com>
2021-09-11 19:13:41 +0000

    Update raise statement six value to 6

--- run.py
+++ run.py
@@ -44,7 +44,7 @@ def validate_data(values):
         [int(value) for value in values]
         if len(values) != 6:
             raise ValueError(
-                f"Exactly six values required, you provided {len(values)}"
+                f"Exactly 6 values required, you provided {len(values)}"
             )
     except ValueError as e:
         print(f"Invalid data: {e}, please try again.\n")
